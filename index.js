3587c039b6c0f838a1e8fda9e686aff087e55449
stanley <47444408+StanleyChan@users.noreply.github.com>
2021-03-19 19:06:50 +0800

    ok to sending data to the db

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const dialogflow = require("dialogflow-fulfillment");
+
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Mongoose"));
 
 app.get("/", (req, res) => {
   res.send("Screaming");
@@ -14,6 +25,7 @@ app.post("/", express.json(), (req, res) => {
 
   function test(agent) {
     agent.add("Sending content from the server");
+
     console.log("sent text content from server");
   }
 
@@ -54,13 +66,35 @@ app.post("/", express.json(), (req, res) => {
         rawPayload: true,
       })
     );
+
     console.log("sent payload from server");
+  }
+
+  function finalConfirmation(agent) {
+    var name = agent.context.get("comfirmation").parameters["person.original"];
+
+    var email = agent.context.get("comfirmation").parameters["email.original"];
+
+    db.collection("test").insertOne({
+      name: name,
+      email: email,
+      time: Date.now(),
+    });
+
+    console.log(name);
+    console.log(email);
+
+    agent.add(
+      `Hello ${name}, your email: ${email}. We confirmed your meeting.`
+    );
   }
 
   var intent = new Map();
 
   intent.set("webhookTest", test);
   intent.set("testServerPayload", payloadTest);
+  intent.set("finalConfirmation", finalConfirmation);
+
   agent.handleRequest(intent);
 });
 
